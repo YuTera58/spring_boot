43ed7f54f3065d14a588e62ae969a6af18a834d5
YuTera58 <YuTerakoya4Study@gmail.com>
2025-11-09 18:23:19 +0900

    Create samurai travel Project

---
+++ samuraitravel/src/main/resources/schema.sql
@@ -0,0 +1,27 @@
+-- schema.sqlファイル：テーブルの作成などに使う
+
+-- INSERT IGNORE INTOとすることで、重複や制約違反が発生した場合でもエラーが発生せず、レコードの追加をスキップしてくれます。
+-- そして、重複や制約違反が発生しない場合は通常どおりレコードを追加してくれます。
+
+-- roles テーブル作成
+CREATE TABLE IF NOT EXISTS roles (
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(50) NOT NULL
+);
+
+-- users テーブル作成
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- 主キー
+    name VARCHAR(50) NOT NULL,
+    furigana VARCHAR(50) NOT NULL,
+    postal_code VARCHAR(50) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role_id INT NOT NULL,                       -- 外部キー
+    enabled BOOLEAN NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (role_id) REFERENCES roles (id)
+);
