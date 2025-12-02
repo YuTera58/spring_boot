1338e9274ff5581bf1786a735756476027bf8299
YuTera58 <YuTerakoya4Study@gmail.com>
2025-12-02 22:58:04 +0900

    Creating administrator login and management pages, creating test code

--- samuraitravel/src/main/java/com/example/samuraitravel/security/WebSecurityConfig.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/security/WebSecurityConfig.java
@@ -27,6 +27,7 @@ public class WebSecurityConfig {
             .authorizeHttpRequests((requests) -> requests
                 //（旧コード）.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/").permitAll()  // すべてのユーザーにアクセスを許可するURL
                 .requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**").permitAll()  // すべてのユーザーにアクセスを許可するURL
+                .requestMatchers("/admin/**").hasRole("ADMIN")  // 管理者にのみアクセスを許可するURL
                 .anyRequest().authenticated()                   // 上記以外のURLはログインが必要（会員または管理者のどちらでもOK）
             )
             .formLogin((form) -> form
