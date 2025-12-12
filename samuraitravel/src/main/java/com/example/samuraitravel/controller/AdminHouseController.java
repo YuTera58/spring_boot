bc4ecb79690830f106c4f3b2d4d56dd514d24f50
YuTera58 <YuTerakoya4Study@gmail.com>
2025-12-13 00:08:24 +0900

    更新機能を実装

--- samuraitravel/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.samuraitravel.entity.House;
+import com.example.samuraitravel.form.HouseEditForm;
 import com.example.samuraitravel.form.HouseRegisterForm;
 import com.example.samuraitravel.service.HouseService;
 
@@ -128,6 +129,61 @@ public class AdminHouseController {
 
         houseService.createHouse(houseRegisterForm);
         redirectAttributes.addFlashAttribute("successMessage", "民宿を登録しました。");
+
+        return "redirect:/admin/houses";
+    }
+    
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable(name = "id") Integer id, RedirectAttributes redirectAttributes, Model model) {
+        Optional<House> optionalHouse = houseService.findHouseById(id);
+
+        if (optionalHouse.isEmpty()) {
+            redirectAttributes.addFlashAttribute("errorMessage", "民宿が存在しません。");
+
+            return "redirect:/admin/houses";
+        }
+
+        House house = optionalHouse.get();
+        
+        // フォームクラスをインスタンス化する
+        //更新前の民宿の各フィールドの値を使ってフォームクラスをインスタンス化し、ビューに渡します。
+        //更新前の民宿画像はビュー内でパスを指定して表示するため（<img th:src="@{/storage/__${house.imageName}__}">）、MultipartFile型のファイルを直接渡す必要はありません。
+        //よって、コンストラクタの引数にはnullを指定します。
+        HouseEditForm houseEditForm = new HouseEditForm(house.getName(), null, house.getDescription(), house.getPrice(), house.getCapacity(), house.getPostalCode(), house.getAddress(), house.getPhoneNumber());
+
+        // 生成したインスタンスをビューに渡す
+        model.addAttribute("house", house);
+        model.addAttribute("houseEditForm", houseEditForm);
+
+        return "admin/houses/edit";
+    }
+    
+    @PostMapping("/{id}/update")
+    public String update(@ModelAttribute @Validated HouseEditForm houseEditForm,
+                         BindingResult bindingResult,
+                         @PathVariable(name = "id") Integer id,
+                         RedirectAttributes redirectAttributes,
+                         Model model)
+    {
+        Optional<House> optionalHouse = houseService.findHouseById(id);
+
+        if (optionalHouse.isEmpty()) {
+            redirectAttributes.addFlashAttribute("errorMessage", "民宿が存在しません。");
+
+            return "redirect:/admin/houses";
+        }
+
+        House house = optionalHouse.get();
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("house", house);
+            model.addAttribute("houseEditForm", houseEditForm);
+
+            return "admin/houses/edit";
+        }
+
+        houseService.updateHouse(houseEditForm, house);
+        redirectAttributes.addFlashAttribute("successMessage", "民宿情報を編集しました。");
 
         return "redirect:/admin/houses";
     }
