a7a11a9e517594562e7e00c9e60198e4030a951a
YuTera58 <YuTerakoya4Study@gmail.com>
2025-12-21 22:00:20 +0900

    会員用詳細編集機能の作成＆管理者用会員詳細編集機能の作成

--- samuraitravel/src/main/java/com/example/samuraitravel/service/UserService.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/service/UserService.java
@@ -1,5 +1,9 @@
 package com.example.samuraitravel.service;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.samuraitravel.entity.Role;
 import com.example.samuraitravel.entity.User;
 import com.example.samuraitravel.form.SignupForm;
+import com.example.samuraitravel.form.UserEditForm;
 import com.example.samuraitravel.repository.RoleRepository;
 import com.example.samuraitravel.repository.UserRepository;
 
@@ -40,6 +45,18 @@ public class UserService {
         return userRepository.save(user);
     }
     
+    @Transactional
+    public void updateUser(UserEditForm userEditForm, User user) {
+        user.setName(userEditForm.getName());
+        user.setFurigana(userEditForm.getFurigana());
+        user.setPostalCode(userEditForm.getPostalCode());
+        user.setAddress(userEditForm.getAddress());
+        user.setPhoneNumber(userEditForm.getPhoneNumber());
+        user.setEmail(userEditForm.getEmail());
+
+        userRepository.save(user);
+    }
+    
     // メールアドレスが登録済みかどうかをチェックする
     // メールアドレス登録済みであれば true を返し、変数 user が null、メールアドレスが未登録であれば false を返す
     public boolean isEmailRegistered(String email) {
@@ -57,5 +74,30 @@ public class UserService {
     public void enableUser(User user) {
         user.setEnabled(true);
         userRepository.save(user);
+    }
+    
+    // メールアドレスが変更されたかどうかをチェックする
+    public boolean isEmailChanged(UserEditForm userEditForm, User user) {
+        return !userEditForm.getEmail().equals(user.getEmail());
+    }
+    
+    // 指定したメールアドレスを持つユーザーを取得する
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+    
+ // すべてのユーザーをページングされた状態で取得する
+    public Page<User> findAllUsers(Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+
+    // 指定されたキーワードを氏名またはフリガナに含むユーザーを、ページングされた状態で取得する
+    public Page<User> findUsersByNameLikeOrFuriganaLike(String nameKeyword, String furiganaKeyword, Pageable pageable) {
+        return userRepository.findByNameLikeOrFuriganaLike("%" + nameKeyword + "%", "%" + furiganaKeyword + "%", pageable);
+    }
+    
+    // 指定したidを持つユーザーを取得する
+    public Optional<User> findUserById(Integer id) {
+        return userRepository.findById(id);
     }
 }
