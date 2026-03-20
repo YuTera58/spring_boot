8b19a7702cc70eeace97c67d1819d631f166918c
YuTera58 <YuTerakoya4Study@gmail.com>
2026-03-21 00:59:39 +0900

    DTO について補足説明(samuraitravel/src/main/java/com/example/samuraitravel/dto/ReservationDTO.java)

--- samuraitravel/src/main/java/com/example/samuraitravel/dto/ReservationDTO.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/dto/ReservationDTO.java
@@ -7,6 +7,14 @@ import lombok.Data;
 
 
 // DTOとはData Transfer Objectの略で、その名のとおりデータを運ぶためのオブジェクトのこと
+// DTO（データ転送オブジェクト）は、レイヤー間（Controller ⇔ Service ⇔ Repository）でデータをやりとりするためのオブジェクト です。
+/* DTOを使う理由
+* 1.エンティティ（DBモデル）とAPIレスポンスを分離できる
+* 2.セキュリティ（パスワードなどの機密情報を隠せる）
+* 3.APIのリクエストやレスポンスの形式を統一できる
+* 4.拡張性が高まり、メンテナンスしやすくなる
+**/
+// DTOを使わずに、エンティティ（DBモデル）を直接APIのレスポンスに使う のは、一般的に避けるべきです。
 @Data
 @AllArgsConstructor
 public class ReservationDTO {
